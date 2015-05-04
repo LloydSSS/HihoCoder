c43f32bd117feacdbb1b98444609ab941646817c
Lloyd <G@GdeMacBook-Pro.local>
2015-05-05 00:19:48 +0800

    hiho template file

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <utility>
+using namespace std;
+
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define rep_st(i,s,t) for (int i = s; i < t; ++i)
+#define rrep(i,n) for (int i = n-1; i >= 0; --i)
+
+typedef long long ll;
+const ll mod=1000000007;
+const int N = 1000010;
+
+void solve() {
+
+}
+
+int main() {
+    freopen("/Users/G/Project/HihoCoder/in.txt", "r", stdin);
+    solve();
+    return 0;
+}
