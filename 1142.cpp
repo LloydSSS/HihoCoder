0731ad96fa2c12bfe59c681262c4214afc9be9e2
Lloyd <lloydsss@126.com>
2015-05-05 14:26:44 +0800

    hiho 1142
    三分搜索
    http://hihocoder.com/problemset/problem/1142

--- 1142.cpp
+++ 1142.cpp
@@ -1,3 +1,5 @@
+//hiho 1142
+//三分搜索，注意dist非凸，需要根据输入x限定搜索范围
 #include <iostream>
 #include <vector>
 #include <string>
@@ -5,6 +7,7 @@
 #include <set>
 #include <algorithm>
 #include <cstdio>
+#include <cmath>
 #include <cstring>
 #include <cstdlib>
 #include <utility>
@@ -15,13 +18,41 @@ using namespace std;
 #define rrep(i,n) for (int i = n-1; i >= 0; --i)
 
 typedef long long ll;
+const double eps = 1e-9;
+
+double a, b, c, x, y;
+double ans;
+
+double func(double n) {
+	return a*n*n+b*n+c;
+}
+
+double dist(double n) {
+	return sqrt((x-n)*(x-n)+(func(n)-y)*(func(n)-y));
+}
 
 void solve() {
-
+//d = min{sqrt((X - x)^2+(aX^2+bX+c-y)^2)}
+	double l = -1e5, r = 1e5;
+	double m = -b/(2.0*a);
+	if (x > m) l = m;
+	else r = m;
+	while (fabs(r-l)>eps) {
+		double lm = l + (r-l)/3;
+		double rm = r - (r-l)/3;
+		if (dist(lm) < dist(rm)) {
+			r = rm;
+		} else {
+			l = lm;
+		}
+	}
+	ans = dist(l);
 }
 
 int main() {
     freopen("D:/Baogui/Project/HihoCoder/in.txt", "r", stdin);
+    scanf("%lf%lf%lf%lf%lf", &a, &b, &c, &x, &y);
     solve();
+    printf("%.3lf\n", ans);
     return 0;
 }
